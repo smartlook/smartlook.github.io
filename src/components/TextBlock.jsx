800de3f0354264d912066265c15ba613425acfa5
Nick S <modexezy@gmail.com>
2020-07-09 10:31:48 +0200

    improvement: TextBlock tweaks

--- src/components/TextBlock.jsx
+++ src/components/TextBlock.jsx
@@ -5,6 +5,10 @@ import { usePlatforms } from "hooks";
 export const TextBlock = ({ visibleOn, invisibleOn, className, children }) => {
   const { currentPlatform } = usePlatforms();
 
+  const showOn = (platform, text) => {
+    return platform === currentPlatform ? text : "";
+  };
+
   let isMatch = false;
 
   if (visibleOn !== undefined) {
@@ -20,8 +24,8 @@ export const TextBlock = ({ visibleOn, invisibleOn, className, children }) => {
   }
 
   if (typeof children === "function") {
-    return <span className={className}>{children(currentPlatform)}</span>;
+    return <p className={className}>{children(showOn)}</p>;
   }
 
-  return <span className={className}>{children}</span>;
+  return <p className={className}>{children}</p>;
 };
