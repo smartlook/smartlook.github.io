99eb02c822108c04738ac59bec53e3cc02ae248b
Pavel Kroh <44899602+pavel-kroh-smartlook@users.noreply.github.com>
2020-08-07 11:23:10 +0200

    Order menus (#19)
    * Polish some installation documentations
    
    * Remove unused docs, update doczrc
    
    * Fixed recording mode -> rendering mode
    
    * Update installation documents for Flutter, iOS and RN
    
    * Fix a link in iOS API ref.
    
    * Polish some react and flutter references
    
    * Main Thread Warning cookbook polishing
    
    * Few more final tweaks to cookbooks and installation manuals
    
    * Some installation docs touches.
    
    * Ordering the menus
    
    * Rename item

--- doczrc.js
+++ doczrc.js
@@ -2,10 +2,17 @@ module.exports = {
 	dest: '/docs',
 	base: '/nextgen-docs',
 	menu: [
-		'SDK Installation',
-		'Cookbooks',
-		'Conceptual',
-		'API reference',
+		'Getting Started',
+		{
+			name: 'SDK Installation',
+			menu: ['Android', 'iOS',]
+		},
+		{
+			name: 'Conceptual',
+			menu: ['SDK Installation', 'Setup & Start Recording', 'When I Will See The Recordings in Dashboard?', 'Life Cycle, Session, User', 'What is recorded and how', 'Handling Sensitive Data', 'Screen Recording Quality', 'Event Tracking', 'Recording Navigation', 'Integrating with Other Tools']
+		  },
+		  'API reference',
+		  'Cookbooks',
 	],
 	title: 'Smartlook',
 	files: 'src/**/*.{md,mdx}',
