10c7bed1dfbb2d6da2036debf75b7a0cd00aed8d
Nick S <modexezy@gmail.com>
2020-08-19 15:09:20 +0200

    chore: smoothscroll polyfill

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import scrollTo from 'scroll-to-element'
 
 import { PlatformsProvider } from 'context'
 
 export const wrapRootElement = ({ element }) => {
 	return <PlatformsProvider>{element}</PlatformsProvider>
+}
+
+export const onClientEntry = () => {
+	require('smoothscroll-polyfill').polyfill()
+}
+
+export const onRouteUpdate = ({ location }) => {
+	if (location.hash) {
+		setTimeout(() => {
+			scrollTo(location.hash, {
+				offset: -100,
+			})
+		}, 0)
+	}
 }
