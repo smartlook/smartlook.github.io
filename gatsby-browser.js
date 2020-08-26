6f6b5aeef7674db1d9f70121a90436c0c3ce0f8b
frantisespurny <frantisek.spurny1@gmail.com>
2020-08-26 15:32:23 +0200

    Syntax highlighting v1

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,19 @@
 import React from 'react'
 import scrollTo from 'scroll-to-element'
+import Prism from 'prism-react-renderer/prism'
 
 import { PlatformsProvider } from 'context'
+
+;(typeof global !== 'undefined' ? global : window).Prism = Prism
+
+require('prismjs/components/prism-kotlin')
+require('prismjs/components/prism-swift')
+require('prismjs/components/prism-groovy')
+require('prismjs/components/prism-xml-doc')
+require('prismjs/components/prism-objectivec')
+require('prismjs/components/prism-typescript')
+require('prismjs/components/prism-dart')
+require('prismjs/components/prism-csharp')
 
 export const wrapRootElement = ({ element }) => {
 	return <PlatformsProvider>{element}</PlatformsProvider>
