96605445b893a1a1ac2f4c87731508de8d0c6f0b
Pavel Pokorný <pavel.pokorny@smartlook.com>
2020-09-10 15:07:51 +0200

    Update SL project key

--- src/gatsby-theme-docz/wrapper.js
+++ src/gatsby-theme-docz/wrapper.js
@@ -4,10 +4,10 @@ const smartlookTrackScript = `
   window.smartlook||(function(d) {
     var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
     var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
-    c.charset='utf-8';c.src='https://recorder.beta.smartlook.com/recorder.js';h.appendChild(c);
+    c.charset='utf-8';c.src='https://recorder.alfa.smartlook.com/recorder.js';h.appendChild(c);
     })(document);
-    smartlook('init', '105bfc83a325ab38e95897cca365d980b9541bf7', {
-        "host": "manager.beta.smartlook.com"
+    smartlook('init', '8706f21ef54df2622b3ced641a0943b918119af4', {
+        "host": "manager.alfa.smartlook.com"
     });
 `
 const Wrapper = ({ children, doc }) => (
