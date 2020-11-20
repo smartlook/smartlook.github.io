62a1e7ad680361c403d06e33bb52bf564f880af5
Pavel Pokorný <pavel.pokorny@smartlook.com>
2020-11-20 14:24:18 +0100

    New SL tracking code

--- src/gatsby-theme-docz/wrapper.js
+++ src/gatsby-theme-docz/wrapper.js
@@ -5,10 +5,10 @@ const smartlookTrackScript = `
   window.smartlook||(function(d) {
     var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
     var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
-    c.charset='utf-8';c.src='https://recorder.alfa.smartlook.com/recorder.js';h.appendChild(c);
+    c.charset='utf-8';c.src='https://recorder.smartlook.com/recorder.js';h.appendChild(c);
     })(document);
-    smartlook('init', '8706f21ef54df2622b3ced641a0943b918119af4', {
-        "host": "manager.alfa.smartlook.com"
+    smartlook('init', 'cd65f1712d0e31062945dcd2169fad5a101f6357', {
+        "host": "manager.eu.smartlook.cloud"
     });
 `
 const Wrapper = ({ children, doc }) => (
