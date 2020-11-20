81ff6109d2d22b69e0cc01f24ce8fe28ebdefcda
Pavel Pokorný <pavel.pokorny@smartlook.com>
2020-11-20 16:38:26 +0100

    Update wrapper.js

--- src/gatsby-theme-docz/wrapper.js
+++ src/gatsby-theme-docz/wrapper.js
@@ -5,7 +5,7 @@ const smartlookTrackScript = `
   window.smartlook||(function(d) {
     var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
     var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
-    c.charset='utf-8';c.src='https://recorder.smartlook.com/recorder.js';h.appendChild(c);
+    c.charset='utf-8';c.src='https://rec.smartlook.com/recorder.js';h.appendChild(c);
     })(document);
     smartlook('init', 'cd65f1712d0e31062945dcd2169fad5a101f6357');
 `
